b040d09a345f00cb5ff651ba01eac69ac67ee40f
hxtk <pgsanders@protonmail.com>
2016-09-04 00:33:46 -0400

    Create graphics files with license headers

---
+++ lib/graphics.h
@@ -0,0 +1,10 @@
+/**********
+ * Header file for Graphics class
+ *
+ * Renders 2d graphics from polygon definitions and locations.
+ *
+ * Copyright: Peter Sanders. All rights reserved.
+ * Date: 2016-09-04
+ */
+
+
